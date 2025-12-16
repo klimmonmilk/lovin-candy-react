3bb6f0e22d71372bfb72327b24323eaafdcd884e
Macaron33-T <tarm3521@gmail.com>
2025-12-16 16:03:35 +0700

    Login Page WIP

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,10 +3,53 @@
 const login = () => {
   return (
     <div>
-      <div>
-        <h1>Login</h1>
-        <input type="email" />
-        <label htmlFor=""></label>
+      <div className="bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
+        <h1 className="text-4xl text-white font-blod text-center mb-6">
+          Login
+        </h1>
+        <form action="">
+          <div className="relative my-4">
+            <input
+              type="email"
+              className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:text-white focus:border-blue-600 peer"
+              placeholder=""
+            />
+            <label
+              htmlFor=""
+              className="abosolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:txt-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Your Email
+            </label>
+          </div>
+          <div className="relative my-4">
+            <input
+              type="password"
+              className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:text-white focus:border-blue-600 peer"
+              placeholder=""
+            />
+            <label
+              htmlFor=""
+              className="abosolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:txt-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Your Password
+            </label>
+          </div>
+          <div>
+            <div>
+              <inpit type="checkbox" name="" id="" />
+              <label htmlFor="Remember Me"></label>
+            </div>
+            <span>Forgot Password?</span>
+          </div>
+          <button className="" type="submit">
+            Login
+          </button>
+          <div>
+            <spn>
+              New Here? <Link to="Register">Create an Account</Link>
+            </spn>
+          </div>
+        </form>
       </div>
     </div>
   );
