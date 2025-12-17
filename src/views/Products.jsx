08250662ee0616ce0c36a944b77ec896213140d5
JackJack2405 <chutikankin7442@gmail.com>
2025-12-17 14:39:36 +0700

    productspage

---
+++ src/views/Products.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/NavBar";
+
+export default function Products() {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+
+}
---
+++ src/views/view.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/NavBar";
+
+export default function View () {
+    return (
+        <div className="Home">
+            <Navbar />
+            
+
+        </div>
+    )
+
+}
